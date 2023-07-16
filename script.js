45d4d04dbdeec21f26a76779110e657d12a59759
Gabe Glasco <gabeglasco@gmail.com>
2023-07-15 19:50:14 -0500

    added delete button function and clear form functionality

--- script.js
+++ script.js
@@ -19,21 +19,27 @@ function handleSubmit(event) {
     event.preventDefault();
     console.log('inside handleSubmit');
 
+    // Retrieve author first name from input box
     const firstNameText = $('#firstName').val();
     console.log("firstNameText", firstNameText);
 
+    // Retrieve author last name from input box
     const lastNameText = $('#lastName').val();
     console.log('lastNameText', lastNameText);
 
+    // Retrieve author id number from input box
     const idNumberText = $('#idNumber').val();
     console.log('idNumberText', idNumberText);
 
+    // Retrieve author job title from input box
     const jobTitleText = $('#jobTitle').val();
     console.log('jobTitleText', jobTitleText);
 
+    // Retrieve author annual salary from input box
     const annualSalaryText = $('#annualSalary').val();
     console.log('annualSalaryText', annualSalaryText);
 
+    // Append new info to dom from captured input
     $('#tableBody').append(`
     <tr>
         <td>${firstNameText}</td>
@@ -44,5 +50,11 @@ function handleSubmit(event) {
         <td><button id="deleteButton">Delete</button></td>
     </tr>`);
 
+    // Clear form by setting the values to an empty string
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#idNumber').val("");
+    $('#jobTitle').val("");
+    $('#annualSalary').val("");
     
 }
